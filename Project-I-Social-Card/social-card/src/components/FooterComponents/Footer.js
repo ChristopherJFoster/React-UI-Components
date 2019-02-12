6cbfa318d98e4e78dca1c4365f736c5d93746e7b
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-02-11 21:54:11 -0500

    Gave the footer buttons some functionality, but I couldn't get the numbers to rerender upon changing (console.log shows that the variables change). I'm guessing I'll learn to do this later in the week.

--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -1,17 +1,53 @@
 import React from "react";
 import "./Footer.css";
 
-const Footer = () => {
-  return (
-    <div className="icon-container">
-      <i className="far fa-comment" />
-      <i className="fas fa-retweet" />
-      <p>6</p>
-      <i className="far fa-heart" />
-      <p>4</p>
-      <i className="far fa-envelope" />
-    </div>
-  );
-};
+class Footer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.retweetCount = 6;
+    this.canRetweet = true;
+    this.likes = 4;
+  }
+
+  commentsAlert() {
+    alert("Comments are disabled at the moment.");
+  }
+
+  retweet = () => {
+    if (this.canRetweet === true) {
+      this.retweetCount += 1;
+      this.canRetweet = false;
+    } else {
+      alert("Hey! You can only retweet once per account.");
+    }
+  };
+
+  like = () => {
+    this.likes += 1;
+    if (this.likes === 7) {
+      alert("Go ahead...like it as many times as you want.");
+    }
+    this.render();
+  };
+
+  email() {
+    alert("Nobody uses email anymore!");
+  }
+
+  render() {
+    return (
+      <div className="icon-container">
+        <i className="far fa-comment" onClick={this.commentsAlert} />
+        <i className="fas fa-retweet" onClick={this.retweet} />
+        <p>{this.retweetCount}</p>
+        <i className="far fa-heart" onClick={this.like} />
+        <p>{this.likes}</p>
+        <i className="far fa-envelope" onClick={this.email} />
+      </div>
+    );
+  }
+}
 
 export default Footer;
+
+console.log(Footer);
